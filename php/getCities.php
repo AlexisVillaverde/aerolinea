7b465094d74dcd4c0a049618f409e0dde7018574
Alexis Villaverde <fv38524@gmail.com>
2024-11-11 21:18:39 -0600

    modifique el archivo get cities para que importe la conexion db

--- php/getCities.php
+++ php/getCities.php
@@ -1,38 +1,21 @@
 <?php
-
-// Habilitar CORS para permitir solicitudes desde localhost:3000
-header("Access-Control-Allow-Origin: http://localhost:3000");
-
-
 // Habilitar CORS para permitir solicitudes desde localhost:3000 y 127.0.0.1:5500
 $allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:5500'];
 
 if (in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
 }
-
-
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-
-// Configura la conexión a la base de datos
-$host = "localhost";
-$user = "root";
-$pass = "SaulGhost04"; //Cambiar por tu contraseña de MariaDB
-$db = "pry_aerolinea";
+include 'conexion_bd.php'; // Incluye el archivo de conexión
 
 try {
-    // Crear una instancia de PDO para la conexión a la base de datos
-    $mysqli = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    // Configurar PDO para que muestre excepciones en caso de error
-    $mysqli->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Obtener el parámetro de búsqueda de la URL
     $query = isset($_GET['query']) ? $_GET['query'] : '';
 
     // Preparar la consulta SQL con un parámetro preparado para evitar inyecciones SQL
-    $stmt = $mysqli->prepare("SELECT Nombre FROM ciudad WHERE Nombre LIKE :query ");
+    $stmt = $conn->prepare("SELECT Nombre FROM ciudad WHERE Nombre LIKE :query ");
 
     // Bindear el parámetro de búsqueda con el valor que incluye comodines para LIKE
     $stmt->bindValue(':query', '%' . $query . '%', PDO::PARAM_STR);
